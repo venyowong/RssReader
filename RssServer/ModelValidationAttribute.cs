7d994e488e204ccd0a8ba74ee2326af7650181b3
venyowong <venyowong@163.com>
2019-10-27 09:45:15 +0800

    RssReader 0.2.2.0 RssServer 0.1.1.0

---
+++ RssServer/ModelValidationAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RssServer
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true)]
+    public class ModelValidationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var modelState = context.ModelState;
+            if (!modelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(modelState);
+            }
+        }
+    }
+}
